a599cb3ebd9e1ad23fd7a727c605910c795ed5de
Edijs <pegijs966@gmail.com>
2022-07-31 11:34:25 +0700

    working with backend api

--- components/DesignInspirations.tsx
+++ components/DesignInspirations.tsx
@@ -2,8 +2,10 @@ import Image from 'next/future/image';
 import { useQuery } from '@tanstack/react-query'
 import { ClipLoader } from 'react-spinners';
 
+import useBefeniClient from '../contexts/befeniContext'
+
 type DesignInspiration = {
-  fabric_code: string | string[] | undefined
+  fabric_code: string
 }
 
 type InspirationImageData = {
@@ -12,28 +14,47 @@ type InspirationImageData = {
   image_url: string,
 }
 
-type InspirationDetails = {
+type InspirationData = {
   id: number,
   shirt_title: string,
   fabric_composition_label: string,
-  image_variants: InspirationImageData[],
+  shirtImages: InspirationImageData[],
 }
 
-type InspirationData = {
-  status: string,
-  data: {
-    collection: [InspirationDetails]
+const getDesignInspirations = async (fabricCode: string, client: any) : Promise<InspirationData[]> => {
+  if (typeof fabricCode === 'undefined') {
+    return Promise.reject(new Error('Invalid fabric code!'))
   }
-}
 
-const getDesignInspirations = async (fabricCode: string | string[] | undefined) : Promise<InspirationData> => {
-  return typeof fabricCode === 'undefined'
-    ? Promise.reject(new Error('Invalid fabric code!'))
-    : fetch(`/api/galery/${fabricCode}`).then(res => res.json());
+  const filters = {
+		fabricIds: [fabricCode],
+		familyIds: [],
+		contrastFamilyIds: [],
+		patterns: [],
+		types: [],
+		weaves: [],
+		compositionIds: [],
+		designs: [],
+		collarStyles: [],
+		cuffStyles: [],
+		myShirtOnly: false,
+		sotwOnly: false
+	};
+
+	const shirts = await client.getShirtGalleryShirts(filters, 'gallery_added_date', 'desc');
+
+	const shirtValues = shirts.getValues();
+
+  console.log(shirtValues );
+  
+  
+  return shirtValues
 }
 
 const DesignInspirations = ({ fabric_code }: DesignInspiration) => {
-  const { data, error, isLoading, isError } = useQuery<InspirationData, Error>(['getDesignInspirations', fabric_code], () => getDesignInspirations(fabric_code))
+  const client = useBefeniClient()
+
+  const { data, error, isLoading, isError } = useQuery<InspirationData[], Error>(['getDesignInspirations', fabric_code], () => getDesignInspirations(fabric_code, client))
   
   if (isLoading) { 
     return (
@@ -50,20 +71,22 @@ const DesignInspirations = ({ fabric_code }: DesignInspiration) => {
       </div>
     )
   }
-
-  console.log(data);
   
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-8 gap-8'>
-      {data.data.collection.map((item, index) => (
-        <div key={item.id} className="w-full text-center">
-          <Image src={item.image_variants.find(variant => variant.size === 'mobile')!.image_url} alt={item.shirt_title} className='rounded-md' width={448} height={477}/>
-          <p className='mt-4 text-xl font-semibold'>{item.shirt_title}</p>
-          <p className='mt-2'>{item.fabric_composition_label}</p>
-          <button className='px-10 py-3.5 bg-slate-200 hover:bg-slate-300 rounded-full mt-4 text-slate-700'>Buy shirt</button>
-        </div>
-      ))}
-    </div>
+    <section className='desgin-inspirations' id="desgin-inspirations">
+      <hr className='mb-8 lg:mt-8'/>
+      <h2 className='text-2xl text-center'>Design Inspirations</h2> 
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-8 gap-8'>
+        {data.map((item, index) => (
+          <div key={item.id} className="w-full text-center">
+            <Image src={item.shirtImages.find(variant => variant.size === 'mobile')!.image_url} alt={item.shirt_title} className='rounded-md' width={448} height={477}/>
+            <p className='mt-4 text-xl font-semibold'>{item.shirt_title}</p>
+            <p className='mt-2'>{item.fabric_composition_label}</p>
+            <button className='px-10 py-3.5 bg-slate-200 hover:bg-slate-300 rounded-full mt-4 text-slate-700'>Buy shirt</button>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- pages/fabrics/[fabric_code].tsx
+++ pages/fabrics/[fabric_code].tsx
@@ -5,11 +5,16 @@ import { useQuery } from '@tanstack/react-query'
 
 // @ts-ignore
 import ReactStars from "react-rating-stars-component";
+import { ClipLoader } from 'react-spinners';
 
-import { Fade } from 'react-slideshow-image';
+import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
 import DesignInspirations from '@/components/DesignInspirations';
+
+import useBefeniClient from '../../contexts/befeniContext'
+
+import { useEffect, useState } from 'react';
 
 type ImageData = {
   id: number,
@@ -21,76 +26,127 @@ type ImageData = {
   updated_at: Date
 }
 
-type FabricDetails = {
+type FabricData = {
   id: number,
   name: string,
   pattern: string,
   comfort: number,
-  images: ImageData[],
+  fabricImages: ImageData[],
   type: string,
-  fiber_1_percentage: number,
-  fiber_1: string,
-  fiber_2_percentage: number,
-  fiber_2: string,
-  fiber_3_percentage: number,
-  fiber_3: string,
+  compositionLabel: {
+    fiber_1_percentage: number,
+    fiber_1: string,
+    fiber_2_percentage: number,
+    fiber_2: string,
+    fiber_3_percentage: number,
+    fiber_3: string,
+  }
   default_weave: string,
   default_weight: string,
   ironing: number
 }
 
-type FabricData = {
-  status: string,
-  data: {
-    collection: [FabricDetails]
+const getFabrics = async (fabricCode: string, client: any) : Promise<FabricData> => {
+  if (typeof fabricCode === 'undefined') {
+    return Promise.reject(new Error('Invalid fabric code!'))
   }
+  // Retrieve the 'Boston' fabric
+  const fabric = await client.getFabric(fabricCode);
+
+  return fabric
+
 }
 
-const getFabrics = async (fabricCode: string | string[] | undefined) : Promise<FabricData> => {
-  return typeof fabricCode === 'undefined'
-    ? Promise.reject(new Error('Invalid fabric code!'))
-    : fetch(`/api/fabrics/${fabricCode}`).then(res => res.json());
-}
+function shortenFabricID(fabricId:string) {
+  const fabricIdNum = parseInt(fabricId);
+	if(fabricIdNum > 10000) {
+		const firstDigit = fabricId.toString()[0];
+		if(firstDigit === '1') {
+			return fabricIdNum % 10000;
+		}
+		if(firstDigit === '2') {
+			return 'T' + (fabricIdNum % 20000);
+		}
+		if(firstDigit === '3') {
+			return 'K' + (fabricIdNum % 30000);
+		}
+	}
+	return fabricId;
+};
 
-function getFabricDetails(item:FabricDetails) {
-  const fabricMaterial = `${item.fiber_1_percentage}% ${item.fiber_1}, ${item.fiber_2_percentage}% ${item.fiber_2} ${item.fiber_3_percentage}% ${item.fiber_3}`;
+function getFabricDetails(item:FabricData) {
+  let fabricCompositionLabelString = '';
+
+  // @ts-ignore
+	const fabricCompositionLabel = item.getFabricCompositionLabel();
+
+	if(fabricCompositionLabel) {
+		fabricCompositionLabelString = fabricCompositionLabel.generateCompositionLabelString();
+	}
+  
+  let fabricWeight: number | string = parseInt(item.default_weight);
+
+	if(!isNaN(fabricWeight)) {
+		fabricWeight = `${fabricWeight} g/sqm`
+  }
 
   const itemDetails = {
     type: item.type,
-    material: fabricMaterial,
+    material: fabricCompositionLabelString,
     webart: item.default_weave,
-    weight: item.default_weight
+    weight: fabricWeight
   }
 
   return itemDetails;
 }
 
+const getCarouselImages = (images: ImageData[]) => {
+  return images.filter((image) => {
+    // @ts-ignore
+		const type = image.getType();
+    // @ts-ignore
+		const variant = image.getVariant();
+		return (type === 'client_shop')
+      || (type === 'konfigurator_base')
+      || ((variant === 'portrait') && (type === 'into_collar' || type === 'fabric_on_roll' || type === 'mannequin'));
+	});
+}
+
 const Fabrics = () => {
   const router = useRouter()
-  const { fabric_code } = router.query
+  const [fabricCode, setFabricCode] = useState('')
+  const client = useBefeniClient()
 
-  const { isLoading, isError, data, error } = useQuery<FabricData, Error>(['getFabrics', fabric_code], () => getFabrics(fabric_code))
+  useEffect(() => {
+    if (router.isReady) {
+      setFabricCode(router.query.fabric_code as string)
+    }
+  }, [router.isReady, router.query]);
+
+  const { isLoading, isError, data, error } = useQuery<FabricData, Error>(['getFabrics', fabricCode], () => getFabrics(fabricCode, client))
 
   if (isLoading) {
-    return <span>Loading...</span>
+    return (
+      <div className='w-full flex justify-center my-16'>
+        <ClipLoader />
+      </div>
+    )
   }
 
   if (isError) {
     return <span>Error: {error.message}</span>
   }
 
-  const mainItem = data.data.collection[0];
-
-  console.log({ ...mainItem });
+  console.log({ data });
   
   return (
-    <main className='fabric w-full flex justify-center'>
-      <div className='container flex flex-col px-4 max-w-screen-xl relative'>
+    <main className='fabric w-full flex justify-center px-4'>
+      <div className='container flex flex-col px-4 max-w-screen-lg relative'>
         <section className='fabric-content'>
-          <div className='title flex my-6 justify-between'>
+          <div className='title flex my-2 lg:my-6 justify-between'>
             <h2 className='flex justify-between portrait:w-full landscape:space-x-2 landscape:lg:w-full text-3xl lg:text-4xl font-semibold'>
-              <span>{mainItem.name}</span>
-              <span>{fabric_code}</span>
+              <span>{data.name}</span>
+              <span>{shortenFabricID(fabricCode)}</span>
             </h2>
 
             <span className='hidden landscape:flex items-center landscape:lg:hidden'>
@@ -99,16 +155,14 @@ const Fabrics = () => {
           </div>
 
           <div className='fabric-info flex flex-col landscape:flex-row lg:flex-row'>
-            <div className='img-carousel w-full landscape:w-1/2 lg:w-1/2'>
-              <Fade>
-                {mainItem.images.filter(img => img.variant === 'portrait').map((fadeImage) => (
-                  <div className="each-fade" key={fadeImage.id}>
-                    <div className="image-container h-80 lg:h-full">
-                      <Image src={fadeImage.image_url} alt="Fabric image" className='object-cover h-[477px] w-full rounded-md' width={624} height={477} />
-                    </div>
+            <div className='img-carousel w-full landscape:w-2/5 lg:w-2/5'>
+              <Slide>
+                {getCarouselImages(data.fabricImages).map((fadeImage) => (
+                  <div className="image-container each-slide max-h-max" key={fadeImage.id}>
+                    <Image src={fadeImage.image_url} alt="Fabric image" className='object-cover h-full aspect-square w-full rounded-md' width={624} height={624} />
                   </div>
                 ))}
-              </Fade>
+              </Slide>
             </div>
 
             <div className={`spacer w-full md:w-8 h-4`} />
@@ -116,7 +170,7 @@ const Fabrics = () => {
             <div className='fabric-details w-full'>
               <table className="table-auto border border-collapse w-full text-left">
                 <tbody>
-                  {Object.entries(getFabricDetails(mainItem)).map((field) => (
+                  {Object.entries(getFabricDetails(data)).map((field) => (
                     <tr key={field[0]} className="border-b even:bg-gray-50 odd:bg-white">
                       <th className="px-4 py-2 capitalize border-r">{field[0]}</th>
                       <td className="px-4 py-2">{field[1]}</td>
@@ -126,26 +180,22 @@ const Fabrics = () => {
               </table>
               <div className='ratings my-16 w-full'>
                 <div className='fabric-ironing px-4 flex space-between items-center'>
-                  <div className='ironing-title text-2xl w-44'>
+                  <div className='ironing-title text-xl lg:text-2xl w-32 lg:w-44'>
                     <span>Ironing</span>
                   </div>
-                  <ReactStars count={5} size={30} value={mainItem.ironing} edit={false} color2='#ffd700' isHalf={true} classNames="text-green-500" />
+                  <ReactStars count={5} size={30} value={data.ironing} edit={false} color2='#ffd700' isHalf={true} classNames="text-green-500" />
                 </div>
                 <div className='fabric-comfort px-4 flex space-between items-center'>
-                  <div className='comfort-title text-2xl w-44'>
+                  <div className='comfort-title text-xl lg:text-2xl w-32 lg:w-44'>
                     <span>Comfort</span>
                   </div>
-                  <ReactStars count={5} size={30} value={mainItem.comfort} edit={false} color2='#ffd700' isHalf={true} />
+                  <ReactStars count={5} size={30} value={data.comfort} edit={false} color2='#ffd700' isHalf={true} />
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <section className='desgin-inspirations' id="desgin-inspirations">
-          <hr className='mb-8 lg:mt-8'/>
-          <h2 className='text-2xl text-center'>Design Inspirations</h2>
-          <DesignInspirations fabric_code={fabric_code} />
-        </section>
+        <DesignInspirations fabric_code={fabricCode} />
       </div>
     </main>
   )
